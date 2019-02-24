1e4808927a9ad86e4f7c0f9b86978d08774a0afe
Mihran Thalhath <mihranz7@gmail.com>
2019-02-24 19:55:39 +0000

    X00T: proprietary: Kang ims.apk from jasmine  * jasmine_sprout 9 PKQ1.180904.001 V10.0.1.0.PDIMIFJ release-keys

--- X00T/Android.mk
+++ X00T/Android.mk
@@ -107,7 +107,7 @@ include $(BUILD_PREBUILT)
 include $(CLEAR_VARS)
 LOCAL_MODULE := imssettings
 LOCAL_MODULE_OWNER := asus
-LOCAL_SRC_FILES := proprietary/app/imssettings/imssettings.apk
+LOCAL_SRC_FILES := proprietary/vendor/app/imssettings/imssettings.apk
 LOCAL_CERTIFICATE := platform
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := APPS
